6ea50990b4e416645471285181f5824caa2cc6e0
Cliff Li <cliff@lin.li>
2018-11-16 06:32:56 +0100

    Load map into state_manager

--- src/flock_simulator/src/state_manager_node.py
+++ src/flock_simulator/src/state_manager_node.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 
 import rospy
+
+import networkx as nx
+import duckietown_world as dw
 from std_msgs.msg import String
 
 
@@ -10,6 +13,10 @@ class StateManagerNode(object):
 
         # Parameters
         self.sim_frequency = 20  # Frequency of simulation in Hz
+
+        # Map
+        self.map = dw.load_map('4way')
+        self.map_graph = self.generateMapGraph(self.map)
 
         # Subscribers
         self.sub_requests = rospy.Subscriber(
@@ -24,14 +31,34 @@ class StateManagerNode(object):
         self.request_timer = rospy.Timer(
             rospy.Duration.from_sec(1.0 / self.sim_frequency), self.cbTimer)
 
-        # other stuff
-
     def updateState(self, event):
         # Update state
 
         # Publish
         msg_state = String()
         self.pub_state.publish(msg_state)
+
+    def generateMapGraph(self, map):
+        tilemap = map.children['tilemap']
+        map_size = [tilemap.H, tilemap.W]
+        graph = nx.Graph()
+        for name, tile in tilemap.children.items():
+            str_parse = name.split("-")
+            if str_parse[0] != "tile" or not tile.drivable:
+                continue
+            graph.add_node(name, coord=[str_parse[1], str_parse[2]])
+        for x in range(map_size[0]):
+            for y in range(map_size[1]):
+                current_tile = "tile-%d-%d" % (x, y)
+                right_tile = "tile-%d-%d" % (x, y + 1)
+                bottom_tile = "tile-%d-%d" % (x + 1, y)
+                if current_tile not in graph:
+                    continue
+                if right_tile in graph:
+                    graph.add_edge(current_tile, right_tile)
+                if bottom_tile in graph:
+                    graph.add_edge(current_tile, bottom_tile)
+        return graph
 
     def cbRequests(self, data):
         # Do something with requests
