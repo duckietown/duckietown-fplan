65910d361a584a25f48bcb76516dc7d80eca79dd
maximilien <maximilien.picquet@gmail.com>
2018-11-25 21:34:25 +0100

    changed structure of dispatcher, initalized request generator, todo include to simulator

--- src/flock_simulator_dispatcher/src/dispatcher_core.py
+++ src/flock_simulator_dispatcher/src/dispatcher_core.py
@@ -1,32 +1,64 @@
 import random
-import numpy as np
-
-def getclosestresquest(requests, pose):
-    closestRequestId = np.find_nearest(requests.locations, pose)
-    return costumerlocation = requests(closestRequestId).start
 
 
-def dispatcher(requests, duckies):
+# TODO Implement duckie initial(t=0) status = 'IDLE'
 
-    for duckies in range(duckie,duckies):
+def dispatcher(open_requests, duckies):
+
+    for duckie in duckies:
+        pose = duckies[duckie]['pose']
+        status = duckies[duckie]['status']
+        target_location = duckies[duckie]['target_location']
+
         #IDLE
-        if duckie.status == 'IDLE':
+        if status == 'IDLE':
 
-            #picking costumer up
-            if costumerlocation == pose:
-                duckie.status == 'WITH_COSTUMER'
-            #going to costumer
+            #find new costumer
+            open_request = getClosestRequest(open_requests,pose)
+            start_loc = open_request['startlocation']
+            end_loc = open_request['endlocation']
+
+            #pick up costumer
+            if start_loc == pose:
+                #change status AND delete open_request
+                status == 'WITH_COSTUMER'
+                target_location = end_loc #put to duckietarget location
+                open_requests.remove(open_request)
+
+            #going to costumer pickup costumer
+            elif start_loc != pose:
+                status == 'GOINGTO_COSTUMER'
+                target_location = start_loc #put to duckietarget location
+
+            #no requests TODO REBALANCE
             else:
-                costumerlocation = getclosestrequest(requests, duckies.id.pose)
+                status == 'IDLE'
+                target_location = pose
 
-        #WREBALANCE TODO LATER
-        #elif duckie.status == 'REBALANCE_DRIVE'
-            #targetlocation = getRebalance(requests, duckies.id.pose)
 
         #WITH COSTUMER
         elif duckie.status == 'WITH_COSTUMER':
-            targetlocation = duckie.request.endlocation
 
-    return duckies #with end location
+            #endlocation reached
+            if pose == target_location:
+                duckie.status == 'IDLE'
+
+    return duckies #with duckietarget location
+
+
+def getClosestRequest(open_requests, pose):
+    if open_requests == 0:
+        return 0 #todo check this case
+    closest_request = open_requests[0]
+    for open_request in open_requests:
+        if dist(pose, open_request) < dist(pose, closest_request):
+            closest_request = open_request
+        return closest_request
+
+
+def dist(pose, request):
+    # TODO DISTANCE REQUEST TO POSE
+    # to measure with rail distances
+    return 10
 
 
