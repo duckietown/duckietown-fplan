32a5fccb3ebd1e539d55569af336adfc091992b4
maximilien <maximilien.picquet@gmail.com>
2018-12-03 16:16:47 +0100

    WIP

--- src/flock_planner/src/dispatcher.py
+++ src/flock_planner/src/dispatcher.py
@@ -9,18 +9,18 @@ class Dispatcher(object):
 
     def update(self, state):
         duckies = state.duckies
-        open_requests = state.open_request
+        open_requests = state.open_requests
 
         for duckie in duckies:
             pose = duckies[duckie]['pose']
             status = duckies[duckie]['status']
-            target_location = duckies[duckie]['target_location']
+            target_location
 
             #IDLE
             if status == 'IDLE':
 
                 #find new costumer
-                open_request = getClosestRequest(open_requests, pose)
+                open_request = self.getClosestRequest(self, open_requests, pose)
                 start_loc = open_request['startlocation']
                 end_loc = open_request['endlocation']
 
@@ -44,18 +44,25 @@ class Dispatcher(object):
             #WITH COSTUMER
             elif duckie.status == 'WITH_COSTUMER':
 
-                #endlocation reached
+                #endlocation reached, request fullfilled
                 if pose == target_location:
                     duckie.status == 'IDLE'
+                    target_location = 0
 
-        return duckies  #with duckietarget location
+        return self.generateCommands(self, pose, target_location)
+
+    def generateCommants(self, pose, target_location):
+        msg = FlockCommand()
+        msg.header.stamp = rospy.Time.now()
+
+
 
     def getClosestRequest(self, open_requests, pose):
         if open_requests == 0:
             return 0  #todo check this case
         closest_request = open_requests[0]
         for open_request in open_requests:
-            if dist(pose, open_request) < dist(pose, closest_request):
+            if self.dist(pose, open_request) < self.dist(pose, closest_request):
                 closest_request = open_request
             return closest_request
 
